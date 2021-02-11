b751a6779e23df27d13b13a6a91a3f74c12036e4
r-fujiyama <hujiyama2843@gmail.com>
2021-02-11 08:35:38 +0000

    update replをparse出来るよう修正

--- repl/repl.go
+++ repl/repl.go
@@ -5,25 +5,68 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
-// prompt prompt
+// prompt > >
 const prompt = "> > "
 
 // Start start REPL
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
 	for {
 		fmt.Println(prompt)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
+
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		_, err := io.WriteString(out, program.String())
+		printIOError(err)
+		_, err = io.WriteString(out, "\n")
+		printIOError(err)
+	}
+}
+
+const monkeyFace = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	_, err := io.WriteString(out, monkeyFace)
+	printIOError(err)
+	_, err = io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	printIOError(err)
+	_, err = io.WriteString(out, " parser errors:\n")
+	printIOError(err)
+	for _, msg := range errors {
+		_, err = io.WriteString(out, "\t"+msg+"\n")
+		printIOError(err)
+	}
+}
+
+func printIOError(err error) {
+	if err != nil {
+		fmt.Println("io.Writer error:" + err.Error())
 	}
 }
